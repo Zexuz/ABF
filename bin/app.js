56b23274b85b88fbdcd1de29ebce2b202e2b03fc
Robin <Robin.edbom@gmail.com>
2016-04-29 22:26:39 +0200

    Finns nu ett gränssnitt i våran pipe server

--- bin/app.js
+++ bin/app.js
@@ -1,13 +1,20 @@
 var http = require('http');
 
 var request = require('request-promise');
+var url = require('url');
 
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    getResponseFromABF(req, function (data) {
-        console.log(data);
+    //we don't have a icon atm and don't want to log that request
+    if (req.url === "/favicon.ico") return;
+
+    //get the query strings as a object
+    var queryObject = url.parse(req.url, true).query;
+
+    getResponseFromABF(queryObject, function (data) {
+        //write the data as a response
         res.end(data);
     });
 }).listen(8080);
@@ -15,10 +22,15 @@ http.createServer(function (req, res) {
 console.log("Server is running on port 8080");
 
 
-function getResponseFromABF(req, callback) {
+function getResponseFromABF(queryObject, callback) {
+    //check if we are going to do a id search or term search.
+    var searchString = queryObject.annonsid === undefined ? "matchning?nyckelord=" + queryObject.matchingsOrd : queryObject.annonsid;
+    console.log("http://api.arbetsformedlingen.se/af/v0/platsannonser/" + searchString);
+
+    //do the request with promises
     request(
         {
-            url: "http://api.arbetsformedlingen.se/af/v0/platsannonser/6669299",
+            url: "http://api.arbetsformedlingen.se/af/v0/platsannonser/" + searchString,
             headers: {
                 'Accept-Language': 'en-US,en;q=0.8,sv;q=0.6',
                 'Accept': 'application/json',
@@ -30,6 +42,7 @@ function getResponseFromABF(req, callback) {
             callback(htmlString);
         })
         .catch(function (err) {
+            console.log(err);
             callback(err);
         });
 }
